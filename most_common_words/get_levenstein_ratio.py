90c89b9a38febae30c9f4873fe2fee05154142fe
Iakimov Sergey <serg.n.y@yandex.ru>
2021-11-23 22:34:31 +0500

    Update get_levenstein_ratio.py

--- most_common_words/get_levenstein_ratio.py
+++ most_common_words/get_levenstein_ratio.py
@@ -1,5 +1,8 @@
 from fuzzywuzzy import process
+from fuzzywuzzy import fuzz
 from get_most_common_words import get_most_common_words, get_most_common_words_dict
+
+#print(fuzz.ratio('Привет мир', 'Привет мир')) - выведет процент соответствия
 
 lol = get_most_common_words(get_most_common_words_dict())
 a = process.extract("кость", lol, limit=1)
